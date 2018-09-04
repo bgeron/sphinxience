5cc775bc52e42088f7742b842df6b7493a5d43bc
Bram Geron <bram@bram.xyz>
2018-09-04 23:12:12 +0100

    Advertise our version in the generated HTML

--- sphinxience/__init__.py
+++ sphinxience/__init__.py
@@ -4,10 +4,14 @@ __all__ = []
 
 import os.path
 
+def update_context(app, pagename, templatename, context, doctree):
+    context["sphinxience_version"] = __version__
+
 def setup(app):
     app.require_sphinx('1.7')
 
     app.add_html_theme('sphinxience',
         os.path.abspath(os.path.dirname(__file__)))
 
-    return {'version': '0.1.2'}
+    app.connect("html-page-context", update_context)
+    return {'version': '0.1.2', "parallel_read_safe": True}
