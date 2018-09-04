ca7c4bda63c109f1c23685f996da2a72eaed25e0
Bram Geron <bram@bram.xyz>
2018-09-05 00:47:23 +0100

    Skip directive. Not tested yet for LaTeX.

--- sphinxience/__init__.py
+++ sphinxience/__init__.py
@@ -4,6 +4,8 @@ __all__ = []
 
 import os.path
 
+SUBMODULES = ["skip"]
+
 def update_context(app, pagename, templatename, context, doctree):
     context["sphinxience_version"] = __version__
 
@@ -14,4 +16,8 @@ def setup(app):
         os.path.abspath(os.path.dirname(__file__)))
 
     app.connect("html-page-context", update_context)
+
+    for submodule in SUBMODULES:
+        app.setup_extension("sphinxience.{}".format(submodule))
+
     return {'version': '0.1.2', "parallel_read_safe": True}
