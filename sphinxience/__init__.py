750a4896f3bbb7d439a1c573b2e1346b0f3639be
Bram Geron <bram@bram.xyz>
2018-09-04 22:58:12 +0100

    Add theme of the same name

--- sphinxience/__init__.py
+++ sphinxience/__init__.py
@@ -2,7 +2,12 @@ __version__ = '0.1.2'
 
 __all__ = []
 
+import os.path
+
 def setup(app):
     app.require_sphinx('1.7')
+
+    app.add_html_theme('sphinxience',
+        os.path.abspath(os.path.dirname(__file__)))
 
     return {'version': '0.1.2'}
